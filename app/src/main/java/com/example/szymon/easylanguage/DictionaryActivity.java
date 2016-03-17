f6d8d4797dd50538822dc444b7d5b154557243b0
Szymon Soppa <sz.soppa@gmail.com>
2016-03-17 01:12:01 +0100

    Adds list action in dictionary activity

--- app/src/main/java/com/example/szymon/easylanguage/DictionaryActivity.java
+++ app/src/main/java/com/example/szymon/easylanguage/DictionaryActivity.java
@@ -1,7 +1,10 @@
 package com.example.szymon.easylanguage;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class DictionaryActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class DictionaryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dictionary);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.dictionary_menu, menu);//Menu Resource, Menu
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_list:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
